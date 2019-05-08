b2b5ba7c134b64c11a3b6841f2fee1ab19a702d9
Dan <danhemerlein@gmail.com>
2019-05-08 10:38:45 -0400

    LOL

---
+++ git_commit_inflator.sh
@@ -0,0 +1,9 @@
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"
+if [ $RANDOM -lt 22936 ] # max random value: 32767
+then
+    fortune | cowsay > README.txt
+    git commit -am "LOL"
+    git push
+else
+    echo skipping today
+fi
